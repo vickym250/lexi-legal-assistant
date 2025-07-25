ea8b0a6bd509177e2b1cf280343f469e4f34d890
Vikram Maurya <162531239+vickym250@users.noreply.github.com>
2025-07-25 08:54:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/lexi-legal-assistant/", // 👈 यह जोड़ना जरूरी है
+ 
   plugins: [react(), tailwindcss()],
 })
